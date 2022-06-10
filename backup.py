5cc9071c1f25ed211cad546f95c5bb9cd4b32441
“emugnier” <emugnier@ucsd.edu>
2022-06-10 11:55:49 -0700

    change log output

--- backup.py
+++ backup.py
@@ -3,8 +3,7 @@ import subprocess
 import sys
 import logging
 import argparse
-
-REMOTE = "secret"
+from time import sleep
 
 # # get_date_first archive
 # def get_date_first_archive():
@@ -19,6 +18,8 @@ REMOTE = "secret"
 
 # dt=date +%Y%m%d.%I%M%S
 # rclone sync /path/to/local/folder remote:backup --backup-dir=remote:archive/$dt
+# TODO add log with time
+stream_handler = None
 
 
 def setup_logging(log_file, log_level):
@@ -26,6 +27,8 @@ def setup_logging(log_file, log_level):
     formatter = logging.Formatter("[ %(levelname)s ] %(message)s")
     handlerFile = logging.FileHandler(log_file)
     handlerOut = logging.StreamHandler(sys.stdout)
+    global stream_handler
+    stream_handler = handlerOut
 
     logging.basicConfig(level=log_level, handlers=[handlerFile, handlerOut])
 
@@ -34,23 +37,28 @@ def execute_command(command):
     logging.info("EXECUTE %s" % command)
 
     res = subprocess.Popen(
-        command,
+        command.split(),
         stderr=subprocess.PIPE,
         stdout=subprocess.PIPE,
-        shell=True,
+        encoding="utf-8",
     )
-    output, err = res.communicate()
+    while True:
+        line_stdout = res.stdout.readline()
+        if line_stdout == "" and res.poll() is not None:
+            break
+        if line_stdout != "":
+            logging.info("RESULT %s", line_stdout)
 
-    logging.info("RESULT %s", output.decode("utf-8"))
-    if err:
-        logging.error(err.decode("utf-8"))
+    line_stderr = res.stderr.readline()
+    if line_stderr != "":
+        logging.error(line_stderr)
 
 
 def sync(remote, path):
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
 
-    command = "rclone sync {path} {remote}:backup --backup-dir={remote}:archive/{dt_string}".format(
+    command = "rclone sync -P {path} {remote}:backup --backup-dir={remote}:archive/{dt_string}".format(
         remote=remote, dt_string=dt_string, path=path
     )
     execute_command(command)
@@ -72,15 +80,17 @@ if __name__ == "__main__":
     main_parser = argparse.ArgumentParser(prog="rclone backup tool")
     subparsers = main_parser.add_subparsers(dest="command", required=True)
     parser_sync = subparsers.add_parser("sync")
+    parser_sync.add_argument("remote", type=str, help="remote")
     parser_sync.add_argument("path", type=str, help="path to the content to backup")
-    parser_sync = subparsers.add_parser("mount")
-    parser_sync.add_argument("path", type=str, help="mount location")
+    parser_mount = subparsers.add_parser("mount")
+    parser_mount.add_argument("remote", type=str, help="remote")
+    parser_mount.add_argument("path", type=str, help="mount location")
 
     args = main_parser.parse_args()
     setup_logging("log.info", 0)
 
     if args.command == "sync":
-        sync(REMOTE, args.path)
+        sync(args.remote, args.path)
 
     if args.command == "mount":
-        mount(REMOTE, args.path)
+        mount(args.remote, args.path)
